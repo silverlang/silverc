2d2bd02853e68094561c19fd4b522a6fc360774b
Angelo Fallaria <ba.fallaria@gmail.com>
2023-09-19 05:33:17 +0800

    feat: add TokenKind enum variants

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -11,9 +11,147 @@ pub struct Token {
 }
 
 pub enum TokenKind {
+    Identifier(Rc<str>),
     IntegerLiteral(Rc<str>),
     StringLiteral(Rc<str>),
-    Identifier(Rc<str>),
+
+    /// \n
+    NewLine,
+
+    Indent,
+
+    Dedent,
+
+    /// "("
+    LParen,
+
+    /// ")"
+    RParen,
+
+    /// "["
+    LBracket,
+
+    /// "]"
+    RBracket,
+
+    /// "{"
+    LBrace,
+
+    /// "}"
+    RBrace,
+
+    /// ":"
+    Colon,
+
+    /// ";"
+    Semi,
+
+    /// "."
+    Dot,
+
+    /// ","
+    Comma,
+
+    /// "+"
+    Plus,
+
+    /// "-"
+    Minus,
+
+    /// "*"
+    Star,
+
+    /// "/"
+    Slash,
+
+    /// "%"
+    Percent,
+
+    /// "^"
+    Caret,
+
+    /// "&"
+    Amper,
+
+    /// "|"
+    Pipe,
+
+    /// "~"
+    Tilde,
+
+    /// "="
+    Equals,
+
+    /// "<"
+    Less,
+
+    /// ">"
+    Greater,
+
+    /// "!"
+    Not,
+
+    /// "@"
+    At,
+
+    /// "->"
+    RArrow,
+
+    /// "=="
+    EqualsEquals,
+
+    /// "!="
+    NotEquals,
+
+    /// "<="
+    LessEquals,
+
+    /// ">="
+    GreaterEquals,
+
+    /// "<<"
+    LShift,
+
+    /// ">>"
+    RShift,
+
+    /// "**"
+    StarStar,
+
+    /// "+="
+    PlusEquals,
+
+    /// "-="
+    MinusEquals,
+
+    /// "*="
+    StarEquals,
+
+    /// "/="
+    SlashEquals,
+
+    /// "%="
+    PercentEquals,
+
+    /// "&="
+    AmperEquals,
+
+    /// "|="
+    PipeEquals,
+
+    /// "^="
+    CaretEquals,
+
+    /// "<<="
+    LShiftEquals,
+
+    /// ">>="
+    RShiftEquals,
+
+    /// "**="
+    StarStarEquals,
+
+    Unknown,
 }
 
 pub struct Cursor<'a> {
