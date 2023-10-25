9ecf532ed2737801a308a7447fde1f4333804a99
Angelo Fallaria <ba.fallaria@gmail.com>
2023-10-25 12:47:03 +0800

    feat(lexer): make first four tests pass

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -1,4 +1,8 @@
-use std::str::Chars;
+use std::{
+    collections::VecDeque,
+    iter::{Enumerate, Peekable},
+    str::Chars,
+};
 
 use crate::span::Span;
 
@@ -160,14 +164,35 @@ pub enum TokenKind {
     Unknown,
 }
 
+use self::TokenKind::*;
+
 pub struct Cursor<'a> {
-    source: Chars<'a>,
+    source: Peekable<Enumerate<Chars<'a>>>,
 }
 
 impl<'a> Cursor<'a> {
     pub fn new(source_code: &'a str) -> Cursor<'a> {
         Cursor {
-            source: source_code.chars(),
+            source: source_code.chars().enumerate().peekable(),
+        }
+    }
+
+    /// [Iterator::take_while] that does not consume non-matching items
+    /// by peeking
+    pub fn take_while(&mut self, predicate: fn(char) -> bool) -> Vec<char> {
+        let mut chars = Vec::new();
+
+        loop {
+            let Some(char) = self.source.peek()
+            else {
+                return chars;
+            };
+
+            if predicate(char.1) {
+                chars.push(self.source.next().unwrap().1);
+            } else {
+                return chars;
+            }
         }
     }
 }
@@ -183,6 +208,8 @@ pub struct Lexer<'a> {
     /// The Lexer will execute each function, and if a function returns [Some] it will create a [Token] with
     /// the [TokenKind] returned.
     custom_rules: &'a [LexerRule],
+
+    token_queue: VecDeque<Token>,
 }
 
 impl<'a> Lexer<'a> {
@@ -191,11 +218,213 @@ impl<'a> Lexer<'a> {
             cursor: Cursor::new(source_code),
             custom_rules: rules::LEXER_RULES,
             src: source_code,
+            token_queue: VecDeque::new(),
         }
     }
 }
 
 impl<'a> Iterator for Lexer<'a> {
     type Item = Token;
-    fn next(&mut self) -> Option<Token> {}
+    fn next(&mut self) -> Option<Token> {
+        if let Some(token) = self.token_queue.pop_front() {
+            return Some(token);
+        }
+
+        let mut end_idx: usize;
+        let mut start_idx: usize;
+        let mut char: char;
+
+        loop {
+            (end_idx, char) = self.cursor.source.next()?;
+            start_idx = end_idx;
+
+            if char == ' ' {
+                continue;
+            }
+
+            break;
+        }
+
+        let token_kind = match char {
+            char if is_ident_start(char) => {
+                let mut chars: Vec<char> = self.cursor.take_while(is_ident_body);
+                end_idx = end_idx + chars.len();
+
+                chars.insert(0, char);
+
+                let ident_str = String::from_iter(&chars);
+                Identifier(ident_str)
+            }
+            char @ '0'..='9' => {
+                let mut chars: Vec<char> = self.cursor.take_while(is_digit);
+                end_idx = end_idx + chars.len();
+
+                chars.insert(0, char);
+
+                let int_str = String::from_iter(&chars);
+                IntegerLiteral(int_str)
+            }
+            '+' => match self.cursor.source.peek() {
+                Some((_, '=')) => {
+                    (end_idx, _) = self.cursor.source.next()?;
+                    PlusEquals
+                }
+                _ => Plus,
+            },
+            '-' => match self.cursor.source.peek() {
+                Some((_, '=')) => {
+                    (end_idx, _) = self.cursor.source.next()?;
+                    MinusEquals
+                }
+                Some((_, '>')) => {
+                    (end_idx, _) = self.cursor.source.next()?;
+                    RArrow
+                }
+                _ => Minus,
+            },
+            '*' => match self.cursor.source.peek() {
+                Some((_, '=')) => {
+                    (end_idx, _) = self.cursor.source.next()?;
+                    StarEquals
+                }
+                Some((_, '*')) => {
+                    (end_idx, _) = self.cursor.source.next()?;
+
+                    match self.cursor.source.peek() {
+                        Some((_, '=')) => {
+                            (end_idx, _) = self.cursor.source.next()?;
+                            StarStarEquals
+                        }
+                        _ => StarStar,
+                    }
+                }
+                _ => Star,
+            },
+            '/' => match self.cursor.source.peek() {
+                Some((_, '=')) => {
+                    (end_idx, _) = self.cursor.source.next()?;
+                    SlashEquals
+                }
+                _ => Slash,
+            },
+            '(' => LParen,
+            ')' => RParen,
+            '[' => LBracket,
+            ']' => RBracket,
+            '{' => LBrace,
+            '}' => RBrace,
+            ':' => Colon,
+            ';' => Semi,
+            '.' => Dot,
+            ',' => Comma,
+            '%' => match self.cursor.source.peek() {
+                Some((_, '=')) => {
+                    (end_idx, _) = self.cursor.source.next()?;
+                    PercentEquals
+                }
+                _ => Percent,
+            },
+            '^' => match self.cursor.source.peek() {
+                Some((_, '=')) => {
+                    (end_idx, _) = self.cursor.source.next()?;
+                    CaretEquals
+                }
+                _ => Caret,
+            },
+            '&' => match self.cursor.source.peek() {
+                Some((_, '=')) => {
+                    (end_idx, _) = self.cursor.source.next()?;
+                    AmperEquals
+                }
+                _ => Amper,
+            },
+            '|' => match self.cursor.source.peek() {
+                Some((_, '=')) => {
+                    (end_idx, _) = self.cursor.source.next()?;
+                    PipeEquals
+                }
+                _ => Pipe,
+            },
+            '~' => Tilde,
+            '=' => match self.cursor.source.peek() {
+                Some((_, '=')) => {
+                    (end_idx, _) = self.cursor.source.next()?;
+                    EqualsEquals
+                }
+                _ => Equals,
+            },
+            '<' => match self.cursor.source.peek() {
+                Some((_, '=')) => {
+                    (end_idx, _) = self.cursor.source.next()?;
+                    LessEquals
+                }
+                Some((_, '<')) => {
+                    (end_idx, _) = self.cursor.source.next()?;
+
+                    match self.cursor.source.peek() {
+                        Some((_, '=')) => {
+                            (end_idx, _) = self.cursor.source.next()?;
+                            LShiftEquals
+                        }
+                        _ => LShift,
+                    }
+                }
+                _ => Less,
+            },
+            '>' => match self.cursor.source.peek() {
+                Some((_, '=')) => {
+                    (end_idx, _) = self.cursor.source.next()?;
+                    GreaterEquals
+                }
+                Some((_, '>')) => {
+                    (end_idx, _) = self.cursor.source.next()?;
+
+                    match self.cursor.source.peek() {
+                        Some((_, '=')) => {
+                            (end_idx, _) = self.cursor.source.next()?;
+                            RShiftEquals
+                        }
+                        _ => RShift,
+                    }
+                }
+                _ => Greater,
+            },
+            '!' => match self.cursor.source.peek() {
+                Some((_, '=')) => {
+                    (end_idx, _) = self.cursor.source.next()?;
+                    NotEquals
+                }
+                _ => Not,
+            },
+            '@' => At,
+            '\n' => NewLine,
+            _ => Unknown,
+        };
+
+        let span = Span::new(start_idx, end_idx + 1);
+
+        if self.cursor.source.peek().is_none() {
+            self.token_queue.push_back(Token {
+                kind: NewLine,
+                span: Span::new(end_idx + 1, end_idx + 2),
+            })
+        }
+
+        return Some(Token {
+            kind: token_kind,
+            span,
+        });
+    }
+}
+
+fn is_ident_start(c: char) -> bool {
+    c == '_' || c.is_ascii_alphabetic()
+}
+
+fn is_ident_body(c: char) -> bool {
+    c == '_' || c.is_ascii_alphanumeric()
+}
+
+fn is_digit(c: char) -> bool {
+    ('0'..='9').contains(&c)
 }
