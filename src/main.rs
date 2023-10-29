1e47a9ea9b82df7fedd79cb31ca834afee53d747
Angelo Fallaria <ba.fallaria@gmail.com>
2023-10-29 22:06:26 +0800

    feat: implement a basic REPL for the lexer

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,33 @@
+use std::io;
+use std::io::prelude::*;
+
+use lexer::Lexer;
+
+use crate::lexer::Token;
+
 mod lexer;
 mod span;
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut buffer = String::new();
+    let stdin = io::stdin();
+    let mut handle = stdin.lock();
+
+    println!("Silver lexer output");
+    loop {
+        print!("> ");
+        std::io::stdout().flush()?;
+
+        buffer.clear();
+        handle.read_line(&mut buffer)?;
+
+        buffer.remove(buffer.len() - 1);
+        let buffer = buffer.replace("\\n", "\n");
+
+        let lexer = Lexer::new(&buffer);
+
+        for token in lexer {
+            println!("{:?}", token);
+        }
+    }
 }
